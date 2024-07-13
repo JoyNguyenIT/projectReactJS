0d5c1961841765a638182c36f641a5aa522f1939
Nguyen <huydt094@gmail.com>
2024-07-13 21:43:42 +0700

    Header Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,63 +1,16 @@
-import logo from './logo.svg';
+
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './component/MyComponent';
-import React from 'react';
-import AddUserInfUserInf from './component/AddUserInf';
-class App extends React.Component {
+import Header from './component/Header/Header';
 
-  // state = {
-  //   name: 'Huy',
-  //   address: 'CTU-CanTho University',
-  //   age: 19
-  // }
 
-  // handleClick = (event) => {
-  //   console.log(this.state.name)
-  //   this.setState({
-  //     name: 'Yahoo!',
-  //     age: Math.floor(Math.random() * 100 + 1)
-  //   });
-  // }
+const App = () => {
 
-  // handleMouseOver(event) {
-  // }
 
-  render() {
-    return (
-      <div>
-        {/* <div>
-           My name is {this.state.name}, {this.state.age} ys, U can call me Evan &amp; I come from {this.state.address}
-         <button onClick={this.handleClick}>Click Me!</button>
-          <button onMouseOver={this.handleMouseOver}>Hover Me</button>
-        </div> */}
-        <MyComponent />
-
-      </div>
-    );
-  }
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
 }
-
-
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello world Yue
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
---
+++ src/component/Header/Header.js
@@ -0,0 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Header = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary" bg="primary" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="#home">Joy Nguyen</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#user">User</Nav.Link>
+                        <Nav.Link href="#admin">Admin</Nav.Link>
+                    </Nav>
+                    <NavDropdown title="Setting" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Log In</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            Log out
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">
+                            Profile
+                        </NavDropdown.Item>
+
+                    </NavDropdown>
+                    <Nav>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
